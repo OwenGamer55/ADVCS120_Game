2503a554451a86fef91c5883d6466d14307070ea
OwenGamer55 <81474983+OwenGamer55@users.noreply.github.com>
2024-10-08 15:27:37 -0300

    Added Checklist

--- main.py
+++ main.py
@@ -3,3 +3,23 @@ import sys
 import time
 
 print("Hi")
+
+"""
+Checklist:
+"""
+
+ChecklistEntries = ["Pygame_window"]
+CompletedEntries = []
+UncompletedEntries = []
+Pygame_window = False
+
+
+def Checklist():
+    for entry in ChecklistEntries:
+        if entry:
+            CompletedEntries.append(entry)
+        elif not entry:
+            UncompletedEntries.append(entry)
+    print(UncompletedEntries)
+
+Checklist()
